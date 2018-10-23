7fb55b1969541ea67f35b3e7366f0ee5f83ec386
Sean Ryan <seanpryan89@gmail.com>
2018-10-23 01:00:24 -0400

    finished add new todo functionality, beginning styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,22 @@
-$("li").click(function (){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
+});
+
+//click on "x" to delete todo
+
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(e){
+	if(e.which === 13){
+		//make new li as last child of ul with text set to user inpput
+		$("ul").append("<li><span>X</span> " + $("#newToDo").val() + "</li>");
+		//remove text from inut
+		$(this).val("");
+	};
 });
